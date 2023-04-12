02d683332c0e8550aae555d6bd4c178de36a9724
Alex <alex.dumbreck@gmail.com>
2023-04-12 01:57:34 +0100

    add sharp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+};
+
+module.exports = nextConfig;
